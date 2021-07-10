1159f16d80c79dc91972775244a8b7a8b9f1334f
cgaafary <cgaafary@gmail.com>
2021-07-10 13:21:34 -0400

    Initial Commit

---
+++ ConcurrentMapPrototype/ConcurrentMapPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrentMapPrototypeApp.swift
+//  ConcurrentMapPrototype
+//
+//  Created by Chris Gaafary on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrentMapPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
